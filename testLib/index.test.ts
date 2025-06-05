4e6bcc26cf959530ba7d82ebb623ca1b6bf15f0f
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-06-05 20:16:01 -0300

    feat: add new test models

--- testLib/index.test.ts
+++ testLib/index.test.ts
@@ -1,7 +1,52 @@
-import {Model, SparkApplication, MadeApplication, DocumentationApplication} from "andes-lib"
+import {Actor, Model, SparkApplication, MadeApplication, DocumentationApplication, Requirement, BussinesRule, FunctionalRequirement, UseCase, Event} from "andes-lib"
 import { test , expect } from 'vitest'
 import fs from "fs"
 import path from "path"
+
+
+const myActor: Actor = {
+    name: "Actor"
+}
+
+const myBusinessRule: BussinesRule = {
+    id: "BR01",
+    description: "Business rule description",
+    priority:"High",
+    depends: []
+}
+
+const myFunctionalRequirement: FunctionalRequirement = {
+    id: "FR01",
+    description:"Functional requirement description",
+    priority:"High",
+    depends: [myBusinessRule]
+}
+
+const myRequirement: Requirement = {
+    id:"1",
+    name:"Requirement 1",
+    description: "This is a requirement",
+    requirement: myFunctionalRequirement,
+    requirements: [myFunctionalRequirement,myBusinessRule]
+}
+
+const myEvent: Event = {
+    id: "Event01",
+    name: "My Event",
+    description: "This is my event description",
+    action: "This is my event action",
+    requirements:[myFunctionalRequirement],
+    depends: [],
+    performer: myActor
+}
+
+const myUseCase: UseCase = {
+    actors: [myActor],
+    depends:[],
+    events:[myEvent],
+    id: "UC01",
+    requirements:[myFunctionalRequirement]
+}
 
 const myModel: Model = {
     project: {
@@ -10,7 +55,11 @@ const myModel: Model = {
         description: "This is a description of my awesome project.",
         architecture: "java"
     },
-    components: [] //Tem que colucar uns modulos aqui para testar
+    components: [
+        myRequirement,
+        myActor,
+        myUseCase
+    ]
 };
 
 //Aqui ele ta definindo o caminho onde o arquivo .spark será gerado
